f0ea33cef519abd6445bc157f15bbb1b838e49ea
Aleksandr <alx8437@mail.ru>
2020-02-27 16:26:33 +0300

    add carousel

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1300,
+        adaptiveHeight: true,
+        autoplay: false,
+        dots: false,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_back.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_next.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 1170,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+
+
+    });
+});
